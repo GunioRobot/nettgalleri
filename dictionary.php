dad98fee84325ae95cecfec0cd755e03c6d001a4
simenaasland <simen.aasl@gmail.com>
2010-10-03 18:34:16 +0200

    Fixed translation of cv.html

--- dictionary.php
+++ dictionary.php
@@ -1,7 +1,40 @@
 <?php
 
-#Skal dette være oppbyggningen for ordbøker - Eirik?
+$dictionary = array();
+$dictionary[""] = array("no" => "", "en" => "");
 
-$dictionary = array("keyword1" => array("no" => "nøkkelord én", "en" => "keyword one"));
+// *** CV translation ***
+
+// Personal information
+$dictionary["name"] = array("no" => "Navn", "en" => "Name");
+$dictionary["born"] = array("no" => "F&oslash;dt", "en" => "Born");
+$dictionary["address"] = array("no" => "Adresse", "en" => "Address");
+$dictionary["phone"] = array("no" => "Telefon", "en" => "Phone");
+$dictionary["mobile"] = array("no" => "Mobiltelefon", "en" => "Mobile");
+
+
+// Tables
+$dictionary["period"] = array("no" => "periode", "en" => "period");
+$dictionary["year"] = array("no" => "&aring;r", "en" => "year");
+$dictionary["education"] = array("no" => "utdanning", "en" => "education");
+$dictionary["fellowship"] = array("no" => "stipend", "en" => "fellowship");
+$dictionary["separateExhibits"] = array("no" => "separatustillinger", "en" => "separate exhibits");
+$dictionary["groupCollectiveExhibits"] = array("no" => "gruppe- og kollektivutstillinger", "en" => "group and collective exhibits");
+$dictionary["provinceExhibits"] = array("no" => "års- og landsdelsutstillinger", "en" => "province exhibits");
+$dictionary["ornamentExhibits"] = array("no" => "utsmykningsoppdrag", "en" => "ornament exhibits");
+$dictionary["tuition"] = array("no" => "undervisning", "en" => "tuition");
+$dictionary["commission"] = array("no" => "oppdrag", "en" => "commission");
+$dictionary["membership"] = array("no" => "medlemskap", "en" => "membership");
+
+// Table summaries
+$dictionary["cvsummary1"] = array("no" => "Utdanningsoversikt med sted og tidsrom.", "en" => "Overview of education, with location and period of time.");
+$dictionary["cvsummary2"] = array("no" => "Oversikt over stipend med &aring;rstall.", "en" => "Overview of fellowship, with year.");
+$dictionary["cvsummary3"] = array("no" => "Separatutstillinger og &aring;rstall.", "en" => "Separate exhibits with year of event.");
+$dictionary["cvsummary4"] = array("no" => "Oversikt over gruppe- og kollektivutstillinger, med &aring;r.", "en" => "Overview of group and collective exhibits with year of event.");
+$dictionary["cvsummary5"] = array("no" => "&Aring;rs- og landsdelsutstillinger med &aring;r.", "en" => "Province exhibits with year of event.");
+$dictionary["cvsummary6"] = array("no" => "Viser utsmykningsoppdrag med &aring;r.", "en" => "Showing ornament exhibits with year of event.");
+$dictionary["cvsummary7"] = array("no" => "Undervisningshistorie med &aring;rstall.", "en" => "Tuition history and year of lecturing.");
+$dictionary["cvsummary8"] = array("no" => "Oppdrag med &aring;r.", "en" => "Commission with year.", "en" => "");
+$dictionary["cvsummary9"] = array("no" => "Viser medlemskapsoversikt.", "en" => "Showing memberships.");
 
 ?>
--- functions.php
+++ functions.php
@@ -9,16 +9,18 @@ if(isset($_COOKIE['language'])) {
 	$lang = $_COOKIE['language'];
 }
 
-
 function translate($filename) {
 	global $lang, $dictionary;
 
 	// Get all keywords
 	$handler = fopen($filename, "r");
 	$contents = fread($handler, filesize($filename));
-	
+	fclose($handler);
+
+	// Get all text that should be translated
 	preg_match_all('/\${([^}]+)}/', $contents, $matches);
 
+	// Look up matches and dictionary and perform translation
 	for($i = 0;$i < sizeof($matches[0]);$i++) {
 		$contents = str_replace($matches[0][$i], $dictionary[$matches[1][$i]][$lang], $contents);
 	}
